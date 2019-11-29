427924afed1964f8b8b0376b9e4d79dd9fe7dd9a
Alex Mubarakshin <muban96@mail.ru>
2019-11-29 14:10:26 +0300

    🛠 Update next config

--- next.config.js
+++ next.config.js
@@ -2,4 +2,25 @@ const assetPrefix = process.env.ASSET_PREFIX || '';
 
 module.exports = {
   assetPrefix,
+
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+    };
+  },
+
+  webpack: (config, { isServer, webpack }) => {
+    config.plugins.push(new webpack.DefinePlugin({
+      __CLIENT__: !isServer,
+      __SERVER__: isServer,
+    }));
+
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
+      }),
+    );
+
+    return config;
+  },
 };
