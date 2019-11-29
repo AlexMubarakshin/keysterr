66ad595675e263d7616ae5137fd971322b436a0e
Alex Mubarakshin <muban96@mail.ru>
2019-11-29 15:13:38 +0300

    ☝️ Change next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 const assetPrefix = process.env.PAGES_ASSET_PREFIX || '';
-const backendUrl = process.env.PAGES_BACKEND_URL || '';
+
+if (!!assetPrefix) {
+  console.log('Configure the app with assetPrefix:', assetPrefix);
+}
 
 module.exports = {
   assetPrefix,
-  'process.env.BACKEND_URL': backendUrl,
 
   exportPathMap: function () {
     return {
